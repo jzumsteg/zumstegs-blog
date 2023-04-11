a2239266da994767d8304cebe9bd99bdeebe7398
John <john@zumsteg.us>
2023-04-11 17:18:59 -0500

    Edits

---
+++ _drafts/new-blog.md
@@ -0,0 +1,10 @@
+---
+title: A New Blog!
+author: JZ
+date: Fri, 06 Apr 2023
+category: 
+math: true
+mermaid: true
+layout: post
+---
+We have a new blog! For several reasons, I've moved to this new blog. Click on the New Blog link at the bottom of the left sidebar.
--- run-drafts.sh
+++ run-drafts.sh
@@ -1,2 +1,2 @@
 echo Running with drafts on :4000
-bundle exec jekyll serve --livereload --host 0.0.0.0 --config "_config.yml,_config_dev.yml" --trace --drafts --watch
+bundle exec jekyll serve --livereload --host --config "_config.yml,_config_dev.yml" --trace --drafts --watch
