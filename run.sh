806c7ba20f0a5de2b3005434204ce2c4374bbe7a
John <john@zumsteg.us>
2023-04-19 19:04:40 +0200

    Typo edits

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 echo Running Zumstegs-travel on 127.0.0.1:4000
-bundle exec jekyll serve --livereload  --config "_config.yml,_config_dev.yml" --trace
+bundle exec jekyll serve --livereload  --host --config "_config.yml,_config_dev.yml" --trace
